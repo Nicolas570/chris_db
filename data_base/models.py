79c05360471a2245575275a240f8c4a871e84a92
Nicolas Charlet <nicolas.charlet570@gmail.com>
2015-09-08 11:35:23 -0400

    adding and modifying fields in tables

--- data_base/models.py
+++ data_base/models.py
@@ -4,12 +4,13 @@ from django.db import models
 
 
 class Patient(models.Model):
-    PatientName = models.CharField(max_length=200,default='')
-    PatientSex = models.CharField(max_length=200,default='')
-    PatientBirthdate = models.DateField(auto_now_add=True)
-    PatientBirthTime = models.DateField(auto_now_add=True)
-    PatientAge = models.CharField(max_length=200,default='')
     PatientId = models.CharField(max_length=200,null=False)
+    PatientName = models.CharField(max_length=200,default='')
+    PatientAge = models.CharField(max_length=200,default='')
+    PatientSex = models.CharField(max_length=200,default='')
+    PatientBirthdate = models.CharField(max_length=200,default='')
+    PatientBirthTime = models.CharField(max_length=200,default='')
+
 
     def __str__(self):
         return self.PatientName
@@ -17,19 +18,19 @@ class Patient(models.Model):
 
 class Study(models.Model):
     StudyName = models.CharField(max_length=200,null=False)
+    Pathology = models.CharField(max_length=200,null=False)
+    StationName = models.CharField(max_length=200,null=False)
+    ManufacturerModelName = models.CharField(max_length=200,null=False)
+    StudyInstanceUID = models.CharField(max_length=200,null=False)
     StudyDate = models.CharField(max_length=200,default='')
     StudyTime = models.CharField(max_length=200,default='')
     AccessionNumber = models.CharField(max_length=200,default='')
     InstitutionName = models.CharField(max_length=200,default='')
     ReferringPhysician =models.CharField(max_length=200,default='')
     PerformingPhysiciansName = models.CharField(max_length=200,default='')
-    Pathology = models.CharField(max_length=200,null=False)
-    StationName = models.CharField(max_length=200,null=False)
-    ManufacturerModelName = models.CharField(max_length=200,null=False)
     ModalitiesInStudy = models.CharField(max_length=200,default='')
     BodyPartExaminated = models.CharField(max_length=200,default='')
     MagneticFieldStrength = models.IntegerField(default=0)
-    StudyInstanceUID = models.CharField(max_length=200,null=False)
     patient = models.ForeignKey(Patient)
 
     def __str__(self):
@@ -39,15 +40,18 @@ class Study(models.Model):
 class Series(models.Model):
     SeriesName = models.CharField(max_length=200,null=False)
     SeriesNumber = models.CharField(max_length=200,null=False)
-    SeriesTime = models.CharField(max_length=200,default='')
-    AcquisitionNumber = models.IntegerField(default=0)
-    ContrastAgent = models.CharField(max_length=200,default='')
-    ScanningSequence = models.CharField(max_length=200,default='')
-    SeriesDescription = models.CharField(max_length=200,null=False)
-    BodyPartExaminated = models.CharField(max_length=200,default='')
     SeriesInstanceUID = models.CharField(max_length=200,null=False)
     ProtocolName = models.CharField(max_length=200,null=False)
     Modality = models.CharField(max_length=200,null=False)
+    AccessionNumber = models.CharField(max_length=200,default='')
+    SeriesDescription = models.CharField(max_length=200,default='')
+    SeriesTime = models.CharField(max_length=200,default='')
+    ContrastAgent = models.CharField(max_length=200,default='')
+    ScanningSequence = models.CharField(max_length=200,default='')
+    SeriesDescription = models.CharField(max_length=200,default='')
+    BodyPartExaminated = models.CharField(max_length=200,default='')
+    AcquisitionNumber = models.IntegerField(default=0)
+
     study = models.ForeignKey(Study)
 
     def __str__(self):
@@ -55,8 +59,8 @@ class Series(models.Model):
 
 
 class MR_Params(models.Model):
-    SliceThickness = models.IntegerField(default=0)
     PixelSpacing = models.CharField(max_length=200,default='')
+    SliceThickness = models.IntegerField(default=0)
     EchoTime = models.FloatField(default=0)
     EchoNumber = models.IntegerField(default=0)
     InversionTime = models.IntegerField(default=0)
