6c1446bf7d2cc25f4e2eccd7bed38879b0bc4a99
Nicolas Charlet <nicolas.charlet570@gmail.com>
2015-09-14 11:13:29 -0400

    adding a clever admin session

--- data_base/admin.py
+++ data_base/admin.py
@@ -2,9 +2,41 @@ from django.contrib import admin
 
 from .models import Patient,Study,Series,MR_Params,US_Params,CT_Params,Review
 
-admin.site.register(Patient)
-admin.site.register(Study)
-admin.site.register(Series)
+class StudyInline(admin.TabularInline):
+    model = Study
+
+
+
+class PatientAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['PatientName']}),
+        ('Patient information', {'fields': ['PatientId', 'PatientAge', 'PatientSex', 'PatientBirthDate'],
+        'classes': ['collapse']},
+        ),
+    ]
+
+    inlines = [StudyInline]
+
+
+class SeriesInline(admin.TabularInline):
+    model = Series
+
+
+class StudyAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['StudyDescription']}),
+        ('Study information', {'fields': ['StationName', 'ManufacturerModelName', 'StudyInstanceUID', 'Pathology',
+        'StudyDate', 'StudyTime', 'AccessionNumber', 'InstitutionName', 'ReferringPhysicianName',
+        'PerformingPhysicianName', 'ModalitiesInStudy'],
+        'classes': ['collapse']},
+        ),
+    ]
+
+    inlines = [SeriesInline]
+
+
+admin.site.register(Patient, PatientAdmin)
+admin.site.register(Study, StudyAdmin)
 admin.site.register(MR_Params)
 admin.site.register(US_Params)
 admin.site.register(CT_Params)
